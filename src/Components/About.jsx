8a8124eb2c682050e68687fb853404f2169c808d
AjithCGVAK <136011578+AjithCGVAK@users.noreply.github.com>
2023-06-09 21:54:29 +0530

    commit third

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,11 @@ import React from "react";
 export default function About(){
 
     return(
-        <h1>Welcome to About page</h1>
+        <React.Fragment>
+            <h1>Welcome to About page</h1>
+            <div style={{backgroundColor: 'blue', fontSize:'20px'}}>fragment example with atribute</div>
+        </React.Fragment>
+        
         
     );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,9 +5,9 @@ import React from "react";
 export default function Home(){
 
     return(
-        <div>
+        <>
         <h1>Welcome to Home page</h1>
         <h1>Welcome to Home page</h1>
-        </div>
+        </>
     );
 }
